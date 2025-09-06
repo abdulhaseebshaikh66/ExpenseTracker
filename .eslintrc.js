7518ba42d15348565f683f4dedba4f7a30fbdf57
Abdul Haseeb <abdulhaseebshaikh66@gmail.com>
2025-09-07 01:56:23 +0300

    libraries installed, folder structure finalized

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+
+  // Custom rules
+  rules: {
+    'react-native/no-inline-styles': 'off',
+    'react-native/no-color-literals': 'off',
+  },
 };
